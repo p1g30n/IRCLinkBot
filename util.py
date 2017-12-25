fc2713f4a75feb548402543aaa36b1c9277dfbeb
Anatol <anatol@riseup.net>
2017-12-25 15:07:11 +0100

    improve title resolution :: fix urlopen 403's + fix html escape

--- util.py
+++ util.py
@@ -116,10 +116,19 @@ def argv(user_com,recv):# returns a named, multidimensional array of on recv
             'ident': ident, 'hostname': host, 'message': message, "command": com}
 
 def gettitle(url):#get the page title of an URL
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+    req = urllib2.Request(url, headers=hdr)
     try:
-        soup = BeautifulSoup(urllib2.urlopen(url).read(1024 * 1024))
+        page = urllib2.urlopen(req)
+        soup = BeautifulSoup(page)
         return soup.title.string
-    except:
+    except urllib2.HTTPError, e:
+        print e.fp.read()
         return False
 
 def geturl(recv):#parse an URL from a string
