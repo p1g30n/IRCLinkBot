6bf98ce20e97e2d0c30bdb25e26c14c0e61d9841
p1g30n <anatol@riseup.net>
2018-01-20 00:32:21 +0100

    !uc display wiki target url

--- plugins/privmsg/uc.py
+++ plugins/privmsg/uc.py
@@ -2,7 +2,7 @@ import urllib2, json
 def main(data):
     if '!uc' in data['recv']:
         args = argv('!uc', data['recv'])
-        if(args['argv'][1:]):
+        if args['argv'][1:]:
             query = '%20'.join(args['argv'][1:])
             wikiajson = urllib2.urlopen(
                 'http://uncovering-cicada.wikia.com/api/v1/Search/List?query=' +
@@ -10,7 +10,8 @@ def main(data):
             ).read()
             wikiaobj = json.loads(wikiajson)
             title = wikiaobj['items'][0]['title']
-            tiny = maketiny(wikiaobj['items'][0]['url'])
-            data['api'].say(args['channel'], title + ' - ' + tiny)
+            url = wikiaobj['items'][0]['url']
+            tiny = maketiny(url)
+            data['api'].say(args['channel'], title + ' - ' + url + ' | ' + tiny + ' ')
         else:
             data['api'].say(args['channel'], "http://uncovering-cicada.wikia.com")
