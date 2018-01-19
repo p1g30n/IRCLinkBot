440349592ceed14366db00fbeddf383f90c799f6
p1g30n <anatol@riseup.net>
2018-01-19 22:51:53 +0100

    point uc! without args to home

--- plugins/privmsg/uc.py
+++ plugins/privmsg/uc.py
@@ -1,13 +1,16 @@
 import urllib2, json
 def main(data):
-    if '!uc ' in data['recv']:
+    if '!uc' in data['recv']:
         args = argv('!uc', data['recv'])
-        query = '%20'.join(args['argv'][1:])
-        wikiajson = urllib2.urlopen(
-            'http://uncovering-cicada.wikia.com/api/v1/Search/List?query=' +
-            query
-        ).read()
-        wikiaobj = json.loads(wikiajson)
-        title = wikiaobj['items'][0]['title']
-        tiny = maketiny(wikiaobj['items'][0]['url'])
-        data['api'].say(args['channel'], title + ' - ' + tiny)
+        if(args['argv'][1:]):
+            query = '%20'.join(args['argv'][1:])
+            wikiajson = urllib2.urlopen(
+                'http://uncovering-cicada.wikia.com/api/v1/Search/List?query=' +
+                query
+            ).read()
+            wikiaobj = json.loads(wikiajson)
+            title = wikiaobj['items'][0]['title']
+            tiny = maketiny(wikiaobj['items'][0]['url'])
+            data['api'].say(args['channel'], title + ' - ' + tiny)
+        else:
+            data['api'].say(args['channel'], "http://uncovering-cicada.wikia.com")
