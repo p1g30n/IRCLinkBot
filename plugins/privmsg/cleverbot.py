39c31a3186c289ef04d4a5f48f6b762e1a880e39
p1g30n <anatol@riseup.net>
2018-07-11 13:27:52 +0200

    add gizoogle to cleverbot

--- plugins/privmsg/cleverbot.py
+++ plugins/privmsg/cleverbot.py
@@ -1,6 +1,7 @@
 def main(data):
     import requests
     import json
+    import bs4
     class CleverBot(object):
         def __init__(self, user, key, nick=None):
             self.user = user
@@ -31,6 +32,17 @@ def main(data):
                 return r['response']
             else:
                 return False
+
+        def gizoogle(self, text):
+            params = {"translatetext": text}
+            target_url = "http://www.gizoogle.net/textilizer.php"
+            resp = requests.post(target_url, data=params)
+            soup_input = re.sub("/name=translatetext[^>]*>/", 'name="translatetext" >', resp.text)
+            soup = bs4.BeautifulSoup(soup_input, "lxml")
+            giz = soup.find_all(text=True)
+            giz_text = giz[39].strip("\r\n")  # Hacky, but consistent.
+            return giz_text
+
     if data['config']['settings']['botNick'] in data['recv']\
             or data['config']['settings']['botNick'].lower() in data['recv']:
         args = argv('', data['recv'])
@@ -38,11 +50,25 @@ def main(data):
         query = query.replace('\n','')
         query = query.replace('\r','')
         client = CleverBot(user='zfGBW2stCf5BgwVn', key='B2zNaEh5upGyWkpvFgvphHkPVinMFthX', nick=data['config']['settings']['botNick'].lower())
-        answer = client.query(query)
+        answer = client.query(query)       
         try:
             if answer:
                 debug = 'Query: ' + query + ' -- Answer: "' + answer + '"'
                 print debug
-                data['api'].say(args['channel'], args['nick'] + ': ' + answer)
-        except:
+                if data['config']['settings']['gizoogle']:
+                    gizquery = answer
+                    gizanswer = client.gizoogle(gizquery)
+                    try:
+                        if gizanswer:
+                            debug = 'Query: ' + gizquery + ' -- Answer: "' + gizanswer + '"'
+                            print debug
+                            data['api'].say(args['channel'], args['nick'] + ': ' + gizanswer)
+                    except Exception as gizerr:
+                        print gizerr
+                        print "error querying gizoogle"
+                        data['api'].say(args['channel'], args['nick'] + ': ' + answer)
+                else:
+                    data['api'].say(args['channel'], args['nick'] + ': ' + answer)
+        except Exception as err:
+            print err
             print "error querying cleverbot"
