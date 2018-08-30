1eb3d231f313767d01b459e9aa30b13834716c72
p1g30n <anatol@riseup.net>
2018-08-30 14:55:53 +0200

    Fix Twitter title fetching

--- util.py
+++ util.py
@@ -116,7 +116,7 @@ def argv(user_com,recv):# returns a named, multidimensional array of on recv
             'ident': ident, 'hostname': host, 'message': message, "command": com}
 
 def gettitle(url):#get the page title of an URL
-    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    hdr = {'User-Agent': 'Wget/1.13.4 (linux-gnu)',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
        'Accept-Encoding': 'none',
