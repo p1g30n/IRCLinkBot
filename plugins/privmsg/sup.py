5c8e36dfb9ccc89d20a5e6219d218f0402853be0
p1g30n <anatol@riseup.net>
2018-09-12 19:16:08 +0200

    respond to greetings

---
+++ plugins/privmsg/sup.py
@@ -0,0 +1,7 @@
+def main(data):
+	sups = ["sup", "hi", "hello", "gday", "good morning", "good night"]
+	for sup in sups:
+		if sup in data['recv'].lower():
+			args = argv(sup, data['recv'])
+			if len(args['argv']) == 1:
+				data['api'].say(args['channel'], sup)
