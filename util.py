049201780e5cfda332ccc9ee22740cb0510bf2a7
p1g30n <anatol@riseup.net>
2019-10-12 16:22:01 +0200

    revert user agent change

--- util.py
+++ util.py
@@ -116,7 +116,7 @@ def argv(user_com,recv):# returns a named, multidimensional array of on recv
             'ident': ident, 'hostname': host, 'message': message, "command": com}
 
 def gettitle(url):#get the page title of an URL
-    hdr = {'User-Agent': 'Googlebot/2.1 (+http://www.google.com/bot.html)',
+    hdr = {'User-Agent': 'Wget/1.13.4 (linux-gnu)',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
        'Accept-Encoding': 'none',
