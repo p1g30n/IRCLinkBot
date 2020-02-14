a4e37d1ad1dcc71f9dbd2969659c1a95e2d7af40
p1g30n <anatol@riseup.net>
2020-02-14 14:23:25 +0100

    match entire string for sups

--- plugins/privmsg/sup.py
+++ plugins/privmsg/sup.py
@@ -1,10 +1,10 @@
 import re
 def main(data):
-	sups = ["sup", "hi", "yo", "hola", "hello", "gday", "good morning", "good night", "kek", "lol", "wat"]
+	sups = ["sup", "hi", "yo", "hola", "hello", "gday", "good morning", "good night"]
 	msg = data['recv'].lower().split(":")[2].rstrip("\n\r")
 	print msg
 	for sup in sups:
-		if msg and msg in sup:
+		if msg == sup:
 			args = argv(sup, data['recv'])
 			if len(args['argv']) == 1:
 				data['api'].say(args['channel'], sup)
