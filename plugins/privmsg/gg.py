cf0ae2d1bd4c9fcb623d2bd00a1a84a4008608a6
p1g30n <anatol@riseup.net>
2020-02-26 17:15:46 +0100

    google search plugin

---
+++ plugins/privmsg/gg.py
@@ -0,0 +1,18 @@
+def main(data):
+    import urllib2
+    from bs4 import BeautifulSoup
+    if '!gg ' in data['recv']:
+        args = argv('!gg', data['recv'])
+        query = ' '.join(args['argv'][1:])
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:73.0) Gecko/20100101 Firefox/73.0')]
+        search_url = 'https://www.google.com/search?hl=en&q=' + query
+        page = opener.open(search_url).read()
+        soup = BeautifulSoup(page, "lxml")
+        text = soup.find_all("span", {"class": "st"})[1].getText()
+        link = soup.find_all("div", {"class": "r"})[0].find("a")
+        if len(text) == 0:
+            data['api'].say(args['channel'], args['nick'] + ': ' + "Nothing bro")
+            return
+        else:
+            data['api'].say(args['channel'], args['nick'] + ': ' + text + " ^ "+ link["href"] +" ^")
