06768c9afcd02f65d75fd3dce4610ecb41c980a9
timepigeon <anatol@riseup.net>
2020-08-13 19:08:33 +0200

    add file extension exceptions for linkgrabber

--- plugins/privmsg/link.py
+++ plugins/privmsg/link.py
@@ -10,7 +10,7 @@ def main(data):
         link = link[0]
         print link;
         # look for title
-        badext = ('.cgi','.pdf', '.mp4', '.avi')
+        badext = ('.cgi','.pdf', '.mp4', '.avi', '.exe', '.dmg', '.zip', '.rar')
         imgext = ('.jpg','.png','.gif','.bmp')
         if not link[-4:].lower() in badext:
             if not link[-4:].lower() in imgext:
